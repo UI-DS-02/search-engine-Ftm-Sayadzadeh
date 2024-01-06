51b5a2e59add959deeb79444e8a6ee3ec5759338
Ftm-Sayadzadeh <ftmdkf@gmail.com>
2024-01-06 23:52:35 +0330

    - Added new functions . These functions are for situations where the given word cannot be found and there is a need to check similar words.

--- search engine/src/OperationManager.java
+++ search engine/src/OperationManager.java
@@ -4,6 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class OperationManager {
     private final HashMap<String, ArrayList<String>> dictionaries;
@@ -25,7 +27,7 @@ public class OperationManager {
 
     // prepare file and delete all char except a-z and A-Z
     public void prepareFiles() throws IOException {
-        for ( File f : files ){
+        for (File f : files) {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(f));
             String text = bufferedReader.readLine();
             if (text != null) {
@@ -33,21 +35,21 @@ public class OperationManager {
                 //String[] split = text.split("[@&#$%/-^_*+<>;:?!\" ,.()\\]\\[|{}]");
                 String[] split = text.split("[^a-zA-Z]");
 
-                for(String s : split){
+                for (String s : split) {
                     /* if map contain this word as key &
                     this file name does not add to this key value before ,
                      it should be added
                      */
-                    if(dictionaries.containsKey(s)){
+                    if (dictionaries.containsKey(s)) {
                         if (!dictionaries.get(s).contains(f.getName())) {
                             dictionaries.get(s).add(f.getName());
                         }
                     }
                     // if this word is new
-                    else{
+                    else {
                         ArrayList<String> values = new ArrayList<>();
                         values.add(f.getName());
-                        dictionaries.put(s , values);
+                        dictionaries.put(s, values);
                     }
                 }
             }
@@ -84,7 +86,7 @@ public class OperationManager {
                 String word = s.substring(1);
                 if (dictionaries.get(word) != null) {
                     // if it is the first list , we should add all dictionary names of this list
-                    if(shouldNotBe.size() == 0)
+                    if (shouldNotBe.size() == 0)
                         shouldNotBe.addAll(dictionaries.get(word));
                         // we should check and just add new dictionaries
                     else {
@@ -103,18 +105,31 @@ public class OperationManager {
                     else
                         shouldBe.retainAll(dictionaries.get(s));
                 } else {
-                    /* this word does not exist in our dictionaries
-                    we should clear all data in shouldBe list :)
-                     */
-                    if (shouldBe.size() != 0)
-                        shouldBe.clear();
+                    // use match word manager func
+                    ArrayList<String> output = this.matchWordManager(s);
+                    // input 0
+                    if (output == null) {
+                        /* this word does not exist in our dictionaries
+                          we should clear all data in shouldBe list :)
+                        */
+                        if (shouldBe.size() != 0)
+                            shouldBe.clear();
+                    }
+                    // consider match word this time
+                    else{
+                        if (shouldBe.size() == 0)
+                            shouldBe.addAll(output);
+                        else
+                            shouldBe.retainAll(output);
+                    }
                 }
             }
         }
         // find result list ------------------------
-        return getResult(atLeast , shouldBe , shouldNotBe) ;
+        return getResult(atLeast, shouldBe, shouldNotBe);
     }
-    public ArrayList<String> getResult(ArrayList<String> atLeast , ArrayList<String> shouldBe , ArrayList<String> shouldNotBe){
+
+    public ArrayList<String> getResult(ArrayList<String> atLeast, ArrayList<String> shouldBe, ArrayList<String> shouldNotBe) {
         ArrayList<String> result = new ArrayList<>();
         if (shouldBe.size() != 0 && atLeast.size() != 0) {
             if (shouldBe.size() < atLeast.size()) {
@@ -139,5 +154,46 @@ public class OperationManager {
         // we should remove elements which exist in notShouldBe list
         result.removeAll(shouldNotBe);
         return result;
+    }
+
+    private ArrayList<String> matchWordManager(String word) {
+        System.out.println(" similar words : " + findMatchingWords(word));
+        System.out.println(" * Enter one of the similar words or enter zero ! ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("* Match Word : ");
+        String input = sc.next();
+        if (!Objects.equals(input, "0"))
+            return this.dictionaries.get(input);
+        else
+            return null;
+    }
+
+    private ArrayList<String> findMatchingWords(String word) {
+        ArrayList<String> similarWords = new ArrayList<>();
+        for (String element : this.dictionaries.keySet()) {
+            // check words with 1 char difference ( same size )
+            if (element.length() == word.length()) {
+                int difference = calculateWordDifference(word, element);
+                if (difference == 1) {
+                    similarWords.add(element);
+                }
+            }
+            // check words with 1 char difference ( different size )
+            else if ((element.contains(word) || word.contains(element))
+                    && Math.abs(element.length() - word.length()) == 1) {
+                similarWords.add(element);
+            }
+        }
+        return similarWords;
+    }
+
+    private int calculateWordDifference(String word, String matchWord) {
+        int difference = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != matchWord.charAt(i)) {
+                difference++;
+            }
+        }
+        return difference;
     }
 }
